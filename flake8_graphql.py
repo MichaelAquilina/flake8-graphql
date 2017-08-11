bfbe98912ab459a10726c2fe153e51ddfbf5353d
Michael Aquilina <michaelaquilina@gmail.com>
2017-08-11 22:20:02 +0100

    Bump version to 0.2.2

--- flake8_graphql.py
+++ flake8_graphql.py
@@ -12,7 +12,7 @@ class GraphQLChecker(object):
     flake8 plugin that lints your graphql strings
     """
     name = 'flake8-graphql'
-    version = '0.2.1'
+    version = '0.2.2'
 
     def __init__(self, tree, *args, **kwargs):
         self.tree = tree
